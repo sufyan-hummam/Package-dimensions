2840e1e3aa3d682f9f08e242b0754f717ef8cf71
Sufyan Hummam <sufyanhummam16@gmail.com>
2025-10-18 23:39:24 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Package-dimensions
---
+++ dimensions.py
@@ -0,0 +1,17 @@
+from math import ceil
+
+def calc_package_dimensions(length, width, height, qty=1, padding=2):
+    package_length = length * qty + padding * 2
+    package_width  = width + padding * 2
+    package_height = height + padding * 2
+    volume = package_length * package_width * package_height
+    return package_length, package_width, package_height, volume
+
+if _name_ == "_main_":
+    print("Enter item dimensions (L W H) in cm:")
+    l, w, h = map(float, input().split())
+    q = int(input("Quantity: "))
+    p = float(input("Padding per side (default 2): ") or 2)
+    result = calc_package_dimensions(l, w, h, q, p)
+    print("Package (LxWxH):", result[:-1])
+    print("Volume:", result[-1])
